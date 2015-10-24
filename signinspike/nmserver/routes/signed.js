a8ae060a0b2b59c16975f7b52bdfecb4004ec651
paulb <paulb@ashridgetech.com>
2015-10-24 15:13:51 +0100

    Skeleton file saver

--- signinspike/nmserver/routes/signed.js
+++ signinspike/nmserver/routes/signed.js
@@ -3,9 +3,9 @@ var express = require('express'),
     google = require('googleapis');
 var router = express.Router();
 
-var AWS_ACCESS_KEY = 'your_AWS_access_key';
-var AWS_SECRET_KEY = 'your_AWS_secret_key';
-var S3_BUCKET = 'images_upload';
+var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
+var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
+var S3_BUCKET = process.env.S3_BUCKET;
 
 var s3 = new aws.S3();
 aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
@@ -14,24 +14,26 @@ aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY}
 /* Signed URL */
 router.post('/', function (req, res, next) {
     var idToken = req.body.idToken;
-    //res.send('Google id for ' + idToken);
+    var filePath = req.body.filePath;
+    var contentType = req.body.contentType;
+    console.log('idToken', idToken, 'filePath', filePath, 'contentType', contentType);
 
-    //var options = {
-    //    Bucket: S3_BUCKET,
-    //    Key: req.body.file_name,
-    //    Expires: 60,
-    //    ContentType: req.body.file_type,
-    //    ACL: 'public-read'
-    //};
-    //
-    //s3.getSignedUrl('putObject', options, function(err, data){
-    //    if(err) return res.send('Error with S3');
-    //
-    //    res.json({
-    //        signed_request: data,
-    //        url: 'https://s3.amazonaws.com/' + S3_BUCKET + '/' + req.body.file_name
-    //    })
-    //})
+    var options = {
+        Bucket: S3_BUCKET,
+        Key: filePath,
+        Expires: 60,
+        ContentType: contentType,
+        ACL: 'public-read'
+    };
+
+    s3.getSignedUrl('putObject', options, function(err, data){
+        if(err) return res.send('Error with S3');
+
+        res.json({
+            signedPutRequest: data,
+            url: 'https://s3.amazonaws.com/' + S3_BUCKET + '/' + filePath
+        })
+    })
 
 });
 
