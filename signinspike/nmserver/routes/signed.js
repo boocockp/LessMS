64c1e91a01ef0639bac4564eef448653d426e767
paulb <paulb@ashridgetech.com>
2015-10-25 11:05:08 +0000

    File saving to S3 working, no id checking

--- signinspike/nmserver/routes/signed.js
+++ signinspike/nmserver/routes/signed.js
@@ -7,8 +7,9 @@ var AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 var AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
 var S3_BUCKET = process.env.S3_BUCKET;
 
+//console.log('AWS_ACCESS_KEY', AWS_ACCESS_KEY, 'AWS_SECRET_KEY', AWS_SECRET_KEY);
+aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY, region: "eu-west-1"});
 var s3 = new aws.S3();
-aws.config.update({accessKeyId: AWS_ACCESS_KEY, secretAccessKey: AWS_SECRET_KEY});
 
 
 /* Signed URL */
@@ -24,14 +25,15 @@ router.post('/', function (req, res, next) {
         Expires: 60,
         ContentType: contentType,
         ACL: 'public-read'
+
     };
 
     s3.getSignedUrl('putObject', options, function(err, data){
-        if(err) return res.send('Error with S3');
+        if(err) return res.status(500).send('Error with S3: ' + err);
 
         res.json({
-            signedPutRequest: data,
-            url: 'https://s3.amazonaws.com/' + S3_BUCKET + '/' + filePath
+            signedPutUrl: data,
+            getUrl: 'http://' + S3_BUCKET + '.s3.amazonaws.com/' + filePath
         })
     })
 
