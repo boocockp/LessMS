6cee7f4a067fe4420563bdf71bf403ac151665b2
paulb <paulb@ashridgetech.com>
2015-12-12 11:57:05 +0000

    WIP - CKEeditorinserting web component into page

--- src/main/ckePlugins/box/plugin.js
+++ src/main/ckePlugins/box/plugin.js
@@ -6,7 +6,9 @@ CKEDITOR.plugins.add( 'box', {
                 var edDoc = editor.document;
 
                 edDoc.getHead().appendHtml('<link rel="import" href="/LessMS/src/main/custom/test-box.html">');
-                editor.insertHtml( '<p>Box here</p><test-box></test-box><p>End of box</p>' );
+                //editor.insertHtml( '<p>Box here</p><test-box></test-box><p>End of box</p>', 'text' );
+                var boxEl = new CKEDITOR.dom.element('test-box');
+                editor.insertElement(boxEl);
             }
         });
         editor.ui.addButton( 'Box', {
